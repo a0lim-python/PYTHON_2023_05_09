20c2f83797cedc0042deee4d1b33888ab65c18df
a0lim <104348646+a0lim@users.noreply.github.com>
2022-07-22 15:54:31 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -2,17 +2,14 @@ from itertools import accumulate
 
 T = int(input()) ## test case
 
-
 for i in range(T):
     k = int(input()) ## floor ## k=3
     n = int(input()) ## number of room ## n=4
     before_k = list(range(1,n+1)) ## [1,2,3,4]
-    current_k = list()
 
     for k_num in range(1,k+1):
-        current_k = list(accumulate(before_k))
-        
-        if k_num != k+1:
-            before_k = current_k    
-    
-    print(current_k[-1])
+        current_k = list(accumulate(before_k)) ## 누적합 
+        if k_num != k+1: ## continue until k-th floor
+            before_k = current_k        
+
+    print(current_k[-1]) ## print k-th floor n-th room
