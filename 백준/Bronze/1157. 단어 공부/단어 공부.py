57d0833c0046f38b271b9bce77e0131238e7862a
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-03 20:21:23 +0900

    feat: add remarks

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,10 +1,10 @@
 import string
 
 word = input()
-word = word.upper()
-unique = list(set(word))
+word = word.upper() ## for counting alphabets
+unique = list(set(word)) ## delete duplication
 
-cnts = []
+cnts = [] ## each letters' count ## int
 
 for i in range(0,len(unique)):
     cnts.append(word.count(unique[i]))
@@ -13,4 +13,4 @@ max = max(cnts)
 if cnts.count(max)!= 1: ## several maxs
     print('?')
 else:
-    print(unique[cnts.index(max)])
+    print(unique[cnts.index(max)]) ## one letter of max
