72c83e8ef4e746acc0508d6abae6e4d57023a4f2
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-04 11:24:15 +0900

    feat: add other codes in 알파벳 찾기.py

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,3 +1,16 @@
+# SOLVED 1
+import string
+
+S = input()
+letters = string.ascii_lowercase
+result = []
+
+for i in range(len(letters)):
+    result.append(S.find(letters[i]))
+
+print(result)
+
+# SOLVED 2
 import string
 
 S = input()
@@ -8,3 +21,18 @@ for i in range(len(letters)):
         print(S.index(letters[i]))
     else:
        print(-1)
+    
+
+# NOT SOLVED
+import string
+
+word = list(input())
+
+alphabet = list(string.ascii_lowercase)  ## a, b, c, ... ,z
+result = [-1 for _ in range(0,len(alphabet))] ## default: -1
+
+for i in range(0,len(result)):
+    letter = word[i]
+    result[alphabet.index(letter)] = word.index(letter)
+ 
+print(result)
