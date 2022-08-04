e001035fe3cd06f2830f1299bbb76fcabc1633d0
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-04 12:44:16 +0900

    feat: add remarks

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -3,12 +3,12 @@ import math
 cnt = int(input())
 
 for i in range(cnt):
-    H, W, N = map(int, input().split())
-    xx = str(math.ceil(N/H))
-    xx = xx.zfill(2)
+    H, W, N = map(int, input().split()) ## height, Width, # of guests
+    xx = str(math.ceil(N/H)) ## round up(올림)
+    xx = xx.zfill(2) ## 1 -> 01 / 10 -> 10
         
     yy = str(N%H)
     if yy == '0':
-        yy = str(H)
+        yy = str(H) ## when N is on top floor(N%H=0) // H
     
     print(int(yy+xx))
