d1d00d9fd3128ab27152502ca256909d1ba5f26a
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-11 19:53:21 +0900

    refactor: add other answer with recursive call

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.py
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.py
@@ -1,3 +1,17 @@
+# 재귀
+N = int(input())
+result = 1
+
+def fact(num: int):
+    if num == 0: ## 0! = 1 // 1 * 0 = 0
+        return 1
+    if num >= 1: ## 종료 조건: num < 1
+        return(num * fact(num - 1)) ## num -= 1
+
+print(fact(N))
+
+
+# for loop
 n = int(input())
 
 def fact(N):
