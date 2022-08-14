995adc446f7c461f83a94b16892d48b4dac12b68
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-14 20:16:59 +0900

    add other answer with recursive_function and two_pointer

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.py
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.py
@@ -1,3 +1,38 @@
+# 재귀 & 투 포인터
+N = int(input())
+
+first = '어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.'
+ask = '"재귀함수가 뭔가요?"'
+story1 = '"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.'
+story2 = '마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.'
+story3 = '그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어."'
+answer = '"재귀함수는 자기 자신을 호출하는 함수라네"'
+last = '라고 답변하였지.'
+
+def recursive(N: int, m = 0, n = 2 * N):
+    ## m: story start pointer, n: answer pointer
+    if m < 0 or n < -1: ## 종료 조건
+        return
+    else: 
+        if m == 0: ## print once
+            print(first)
+
+        if m < n:
+            print('____' * m + ask)
+            print('____' * m + story1)
+            print('____' * m + story2)
+            print('____' * m + story3)
+        elif n == m:
+            print('____' * m + ask)
+            print('____' * m + answer)
+        else:
+            print('____' * (n+1) + last)
+        
+        return recursive(N, m + 1, n - 1)
+        
+recursive()
+
+
 # 재귀
 N = int(input())
 
@@ -33,7 +68,7 @@ print('____' * N + answer)
 last_answer(N) # 구문을 나누어 def를 만들고 순서에 맞게 print함
 
 
-## for loop
+# for loop
 cnt = int(input())
 
 first = '어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.'
