74fccc39aba8243fa39425a4e2a9f8b15d8b8407
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-15 14:59:06 +0900

    [Silver V] Title: 소수 찾기, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.py
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -1,20 +1,19 @@
+# 소수찾기 2: for loop
 N = int(input())
 prime = list(map(int, input().split()))
 prime_num = 0
 
-def is_prime(n: int, i = 2, tf = 0):
-    if n < 2 or i > n: ## 종료 조건
-        return
-    if n % i == 0:
-        if i == n:
-            tf = 1
-            return tf
-        else:
-            return ## not prime
-    return is_prime(n, i+1, tf)
- 
-for i in range(N):
-    if is_prime(prime[i]) == 1:
+def is_prime(n: int):
+    if n < 2:
+        return False
+    else: 
+        for i in range(2, n):
+            if n % i == 0:
+                return False
+        return True
+
+for p in range(N):
+    if is_prime(prime[p]) == True:
         prime_num += 1
-        
+
 print(prime_num)
