1b1bb81e08462363e403ed90fac77391d7b4ec0f
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-15 15:02:40 +0900

    [Silver V] Title: 소수 찾기, Time: 68 ms, Memory: 32952 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.py
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -1,4 +1,7 @@
-# 소수찾기 2: for loop
+# 소수찾기3: 제곱근
+## O(n/2)
+import math
+
 N = int(input())
 prime = list(map(int, input().split()))
 prime_num = 0
@@ -6,11 +9,10 @@ prime_num = 0
 def is_prime(n: int):
     if n < 2:
         return False
-    else: 
-        for i in range(2, n):
-            if n % i == 0:
-                return False
-        return True
+    for i in range(2, int(math.sqrt(n))+1): ## 제곱근까지만 나누기(이전에 없으면 이후에도 없음)
+        if n % i == 0:
+            return False
+    return True
 
 for p in range(N):
     if is_prime(prime[p]) == True:
