b0044c67fbe4f318131c7a53ad803735b4f18d06
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-18 10:29:34 +0900

    refactor: add remark

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -6,12 +6,12 @@ def primes(n: int):
     arr = [True] * (n + 1)
     arr[0] = False
     arr[1] = False
-    answer = []
-    if n >= 2:
+    answer = [] ## print primes
+    if n >= 2: ## if n =1; range error
         for i in range(2, n+1):
             if arr[i] is True:
-                answer.append(i)
-                j = 2
+                answer.append(i) ## i: prime number
+                j = 2 ## start to 2 (not include self)
                 while i * j <= n:
                     arr[i * j] = False
                     j += 1
