81a62fdb7a8b798413f7c3a6d905248575f496af
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-18 18:44:29 +0900

    refactor: add remarks

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
@@ -2,7 +2,7 @@ import math
 
 num = int(input())
 
-def is_prime(n: int):
+def is_prime(n: int): ## prime number: True
     if n <= 1:
         return False
     if n == 2 or n == 3:
@@ -14,13 +14,13 @@ def is_prime(n: int):
             
 for i in range(0,num):
     n = int(input())
-    start = int(math.floor(n / 2))   
-    while start > 0:
-        if is_prime(start) == True:
-            if is_prime(n - start) == True:
-                print(start, n - start)
+    start = int(math.floor(n / 2)) ## n is sum of two number -> try half of n, then try other half   
+    while start > 1:
+        if is_prime(start) == True: ## if number 1 is prime;
+            if is_prime(n - start) == True: ## if number 2 is prime;
+                print(start, n - start) ## start < n - start (not need sort)
                 break
             else:
-                start -= 1
+                start -= 1 ## froma mean to 2
         else:
             start -= 1
