0409948e16dca51e71f4c853986083e8ae283fdc
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-19 17:41:07 +0900

    [unrated] Title: 성격 유형 검사하기, Time: 0.34 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -0,0 +1,22 @@
+def solution(survey, choices):
+    answer = []
+    pers = 'RTCFJMAN'
+    pers_num = [0] * 4 ## 왼쪽 유형: 양수 / 오른쪽 유형: 음수로 표현
+    point = list(range(3,-4,-1)) ## 3 ~ -3
+    
+    for i in range(len(survey)):
+        idx = pers.index(survey[i][0])
+        if idx % 2 == 0:
+            pers_num[int(idx // 2)] += point[choices[i] - 1] ## 홀수: 왼쪽 -> 양수
+        else:
+            pers_num[idx // 2] -= point[choices[i] - 1] ## 짝수: 오른쪽 -> 음수
+            
+    for i in range(len(pers_num)):
+        if pers_num[i] >= 0: ## 사전식 순: R / C / J / A (왼쪽 유형)
+            answer.append(pers[2 * i])
+        else:
+            answer.append(pers[2 * i + 1])
+            
+    answer = ''.join(answer)
+
+    return answer
