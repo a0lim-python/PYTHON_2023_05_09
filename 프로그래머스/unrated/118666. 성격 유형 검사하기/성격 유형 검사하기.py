c113f4e63d61a42a24f264f50d43fdbef8337443
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-19 17:44:51 +0900

    refactor: add remarks

--- 프로그래머스/unrated/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/unrated/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -5,18 +5,18 @@ def solution(survey, choices):
     point = list(range(3,-4,-1)) ## 3 ~ -3
     
     for i in range(len(survey)):
-        idx = pers.index(survey[i][0])
+        idx = pers.index(survey[i][0]) # 해당하는 성격 유형: 왼쪽 알파벳
         if idx % 2 == 0:
             pers_num[int(idx // 2)] += point[choices[i] - 1] ## 홀수: 왼쪽 -> 양수
         else:
             pers_num[idx // 2] -= point[choices[i] - 1] ## 짝수: 오른쪽 -> 음수
             
     for i in range(len(pers_num)):
-        if pers_num[i] >= 0: ## 사전식 순: R / C / J / A (왼쪽 유형)
+        if pers_num[i] >= 0: ## 사전식 순: R / C / J / A (왼쪽 유형) -> 값이 같은 경우에는 왼쪽 유형 선택
             answer.append(pers[2 * i])
         else:
             answer.append(pers[2 * i + 1])
             
-    answer = ''.join(answer)
+    answer = ''.join(answer) ## list to str
 
     return answer
