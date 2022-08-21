160ae94171213120ab84ca55a1f39af910d3921c
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-21 14:47:43 +0900

    refactor: add someone else's answer
    겉으로 보이는 규칙보다 더 깊게 생각하자

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
@@ -6,3 +6,11 @@ def solution(s):
         i = len(s) // 2 ## len = 4; 1:2 -> 2 = 4 // 2
         answer = s[i - 1:i + 1]
     return answer
+
+# someone else's answer
+def string_middle(str):
+
+    return str[(len(str)-1)//2:len(str)//2+1]
+
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+print(string_middle("power"))
