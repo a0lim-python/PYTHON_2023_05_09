1d14de8b7978129b8822c6a0a146e920b160d956
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-21 14:34:04 +0900

    refactor add my other answer
    - 재귀 연습으로 짜봤는데, timeout이 나와서 for로 다시 풀었다.
    - 다른 사람 풀이를 보니, lambda, zip 등의 함수가 코드를 단순화하는 데에 많이 쓰이는 것으로 보인다. 나중에 제대로 알아두면 유용하게 쓸 수 있겠다.

--- 프로그래머스/lv1/70128. 내적/내적.py
+++ 프로그래머스/lv1/70128. 내적/내적.py
@@ -3,3 +3,17 @@ def solution(a, b):
     for i in range(len(a)):
         answer += a[i] * b[i]
     return answer
+
+## 재귀
+def solution(a, b):
+    
+    def dot(a, b, i = 0, ans = 0):
+        if i == len(a): ## 중지 조건
+            return ans
+        else:
+            ans += a[i] * b[i]
+            i += 1
+            return dot(a, b, i = i, ans = ans)
+
+    answer = dot(a, b)
+    return answer
