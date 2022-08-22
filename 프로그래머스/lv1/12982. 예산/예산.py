8739b2cdb9410f0ba891e3a53c7cbbe93e488f6b
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-22 10:34:32 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.py
+++ 프로그래머스/lv1/12982. 예산/예산.py
@@ -1,10 +1,8 @@
 def solution(d, budget):
-    if sum(d) <= budget:
-        answer = len(d)
-    else:
-        d = sorted(d)
-        while sum(d) > budget:
-            d.pop()
-        answer = len(d)
+
+    d = sorted(d)
+    while sum(d) > budget:
+        d.pop()
+    answer = len(d)
     
     return answer
