256e7d9253dcd49bafb42e7f3ec85ac4c22c0e13
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-22 18:30:20 +0900

    refactor: add remarks and someone else's answer

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.py
@@ -1,11 +1,18 @@
+# sqrt -> n의 값이 클수록 효율적
+
 def solution(n):
     import math
     
     answer = 0
-    for i in range(1, math.floor(math.sqrt(n))+ 1):
+    for i in range(1, math.floor(math.sqrt(n))+ 1): ## 약수의 작은 값만 추출/더함 & 상대되는 약수 더함
         if n % i == 0:
-            if i != n // i:
+            if i != n // i: ## 중복 제거
                 answer += i + (n // i)
             else:
                 answer += i
     return answer
+
+# someone else's answer: n // 2 -> 중복 제거 불필요
+
+def sumDivisor(num):
+    return num + sum([i for i in range(1, (num // 2) + 1) if num % i == 0]) ## n의 절반만 추출: (1을 제외하고) 최소가 되는 약수 = 2 -> 최대 약수 = n // 2 -> 그 이상의 숫자는 약수가 될 수 없음
