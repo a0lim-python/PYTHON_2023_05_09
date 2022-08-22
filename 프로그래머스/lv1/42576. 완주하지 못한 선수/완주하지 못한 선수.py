cbfaa88ff2a559c5799a1f62c501d10d7673d017
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-22 09:49:08 +0900

    refactor: add my other answer and someone else's answer

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,4 +1,5 @@
-# collections.Counter + 2 case
+# collections.Counter + set + 2 case
+
 def solution(participant, completion, answer = None):
     import collections
     if set(participant) != set(completion): ## 비완주자가 동명이인이 아닌 경우
@@ -11,3 +12,25 @@ def solution(participant, completion, answer = None):
                 answer = i
                 break   
     return answer
+
+# collections.Counter: 비완주자가 동명이인인 경우에 for 문을 돌리기 때문에, 두 경우 모두 한 번에 판별
+
+def solution(participant, completion):
+    import collections
+
+    ls = list(set(participant))
+    p = collections.Counter(participant); c = collections.Counter(completion)
+    for i in ls:
+        if p[i] != c[i] or c[i] == 0: ## 비완주자가 동명이인인 경우 / 아닌 경우
+            answer = i
+            break
+
+    return answer
+
+# someone else's answer: 
+
+import collections
+
+def solution(participant, completion):
+    answer = collections.Counter(participant) - collections.Counter(completion) ## collections.Counter에서 - 연산이 가능
+    return list(answer.keys())[0]
