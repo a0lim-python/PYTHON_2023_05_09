ab2866f6ac5c7449d2f5f533613d355272fae72e
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-22 16:16:25 +0900

    refactor: add remarks

--- 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.py
+++ 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.py
@@ -1,5 +1,7 @@
+# pop
+
 def solution(n):
-    ans = list(str(n))
+    ans = list(str(n)) ## int 형태로는 list 변환 불가능
     answer = []
     for i in range(len(ans)):
         answer.append(int(ans.pop()))
