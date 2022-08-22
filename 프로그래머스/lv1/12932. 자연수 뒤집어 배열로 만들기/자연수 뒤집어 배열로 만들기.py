fd9f66b0e3976ca6d11f00cc43d5ab0cc2aa60c9
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-22 16:19:43 +0900

    refactor: add someone else's answers

--- 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.py
+++ 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.py
@@ -6,3 +6,15 @@ def solution(n):
     for i in range(len(ans)):
         answer.append(int(ans.pop()))
     return answer
+
+# someone else's answer 1: reversed
+
+def solution(n):
+    return list(map(int, reversed(str(n)))) ## reversed: str 뒤집기
+
+# someone else's answer 2: [::-1]
+
+def solution(n):
+    n = str(n)[::-1] ## str[::-1]: str 뒤집기
+    answer = list(map(int, n))
+    return answer
