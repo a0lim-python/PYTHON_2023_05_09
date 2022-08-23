2b55e2ccb65b01554acc06264d7cd542a449fef3
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-23 13:56:58 +0900

    refactor: add remarks and someone else's answer
    간단한 문제도 다시 한 번 더 생각해보기

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,3 +1,5 @@
+# i // 2를 이용해 약수 구하기
+
 def solution(left, right):
     answer = 0
     
@@ -12,4 +14,15 @@ def solution(left, right):
         else: ## 홀수
             answer -= i
         
+    return answer
+
+# someone else's answer: 제곱근이 정수인 경우, 약수의 개수는 홀수 개
+
+def solution(left, right):
+    answer = 0
+    for i in range(left,right+1):
+        if int(i**0.5)==i**0.5:
+            answer -= i
+        else:
+            answer += i
     return answer
