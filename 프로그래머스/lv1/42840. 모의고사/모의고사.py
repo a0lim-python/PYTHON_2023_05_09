b23c5759dc06488727445a45e8da72beab55ca0b
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-24 20:38:18 +0900

    refactor: add my answer and correct one to someone else's
    구상 순서
    1. 학생별 for loop & % -> 정답 개수 세기
    2. ans에 정답 개수 입력
    3. 정답 수로, 같은 경우에는 1,2,3번 학생 순으로 정렬
    4. 정렬된 학생 번호 출력
    
    너무 복잡하게 생각했다
    정답 수가 높은 순으로 정리하게 되면 어떤 학생의 정답인지 알 수 없게 된다는 생각에 dict도 써보고 sort, index 등을 다 섞어서 쎴다.
    index를 저장하는 방법을 공부해야겠다.
    
    # someone else's answer
    enumerate 함수를 사용하면 index와 value를 동시에 저장하고 사용할 수 있었다.
    자주 사용해서 익숙해져야겠다.

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -1,11 +1,31 @@
+# dict & % & sort lambda
+
+rule = dict()
+rule[1] = [1, 2, 3, 4, 5]
+rule[2] = [2, 1, 2, 3, 2, 4, 2, 5]
+rule[3] = [3, 3, 1, 1, 2, 2, 4, 4]
+ans = dict() ## 맞은 개수 입력
+
+for i in rule.keys(): ## 1, 2, 3번 학생별
+    sol = 0
+    for j in range(len(answers)):
+        if answers[j] == rule[i][j % len(rule[i])]:
+            sol += 1
+    if sol != 0:
+        ans[i] = sol
+
+answer = sorted(ans.items(), key = lambda x: (-x[1], x[0])) ## sort(max(정답), 사전순(1,2,3))
+answer = list(map(int, dict(answer).keys()))
+
+# someone else's answer: enumerate
 def solution(answers):
     p = [[1, 2, 3, 4, 5],
          [2, 1, 2, 3, 2, 4, 2, 5],
          [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
     s = [0] * len(p)
 
-    for q, a in enumerate(answers):
-        for i, v in enumerate(p):
+    for q, a in enumerate(answers): ## 문제 정답 -> q: index, a: value
+        for i, v in enumerate(p): ## 학생들의 답안 -> i: index, v: value
             if a == v[q % len(v)]:
                 s[i] += 1
-    return [i + 1 for i, v in enumerate(s) if v == max(s)]
+    return [i + 1 for i, v in enumerate(s) if v == max(s)] ## 학생들의 정답 수 -> i: index, v: value ## 학생 번호 출력
