200c9ba7088cd8926b4d68a59e94b86d0d265597
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-24 19:55:51 +0900

    refactor: add remarks
    구상 순서
    1. 조합 -> combinations -> TimeOut
    2. 다시 생각 -> 뽑을 수 있는 종류의 최대 수
    3. set -> 중복 삭제 -> 종류의 수
    
    처음 보고 조합 문제라고 생각해서 import도 해서 구했는데, timeout이 떴다.
    다시 처음부터 생각하니, 모든 조합을 나타낼 필요 없이 뽑을 수 있는 종류의 최대 수를 구하면 되는 문제였다.
    속단하지 말고 다시 생각하자

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,7 +1,9 @@
+# 최대 조합의 수
+
 def solution(nums):
-    if len(set(nums)) < len(nums) // 2:
+    if len(set(nums)) < len(nums) // 2: ## 뽑는 수보다 종류가 더 적은 경우
         answer = len(set(nums))
     else:
-        answer = len(nums) // 2
+        answer = len(nums) // 2 ## 뽑는 수만큼 조합이 완성
         
     return answer
