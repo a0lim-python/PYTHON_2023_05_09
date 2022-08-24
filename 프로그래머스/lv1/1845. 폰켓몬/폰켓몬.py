1eeccc257a0147ceac74dc1d363de908439d262c
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-24 19:56:53 +0900

    refactor: add someone else's answer
    더욱 더 간결하게

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -7,3 +7,8 @@ def solution(nums):
         answer = len(nums) // 2 ## 뽑는 수만큼 조합이 완성
         
     return answer
+
+# someone else's answer
+
+def solution(ls):
+    return min(len(ls)/2, len(set(ls))) ## if 구문 대신 min으로 값 구함
