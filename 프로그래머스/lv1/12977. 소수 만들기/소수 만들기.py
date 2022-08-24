d8af37d13e8e244a56802dedc1b96753f58b03d7
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-24 18:30:49 +0900

    add someone else's answers
    someone else's answer 1을 보면
    사중 loop까지 가는데 (소수 판별 포함), 시간복잡도는 매우 높아지겠지만 내 풀이보다 훨씬 깔끔하다.
    
    someone else's answer 2를 보면
    combinations라는 함수를 사용했다.
    이 함수 하나로 삼중 for loop를 대신할 수 있다.

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
@@ -30,3 +30,31 @@ def solution(nums):
             answer += sum[i] ## i의 개수만큼 더함
 
     return answer
+
+# someone else's answer 1: 사중 for loop안에서 소수 판별, 소수의 개수 세기를 한번에 처리
+
+def solution(nums):
+    answer = 0
+
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            for k in range(j+1, len(nums)):
+                number = nums[i] + nums[j] + nums[k]
+                if len([m for m in range(2, number) if number % m == 0]) == 0: ## 소수 판별
+                    answer += 1 # 소수의 개수 더함
+
+    return answer
+
+# someone else's answer 2: combinations (하나의 리스트에서 모든 조합을 구함)
+
+def solution(nums):
+    from itertools import combinations as cb
+    answer = 0
+    for a in cb(nums, 3): ## 세 개의 수를 이용한 조합을 구함
+        cand = sum(a)
+        for j in range(2, cand): ## 소수 판별
+            if cand%j==0:
+                break
+        else:
+            answer += 1
+    return answer
