9967713b63d6728ec357928717ce78d6f01a9f8d
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-24 13:13:06 +0900

    refactor: add someone else's answer
    이 풀이는 이중 for loop을 사용해서 board에 있는 모든 수를 순회한다.
    그렇기 때문에 이 풀이에서는 내 풀이에서 0을 제거하는 for 문을 사용하지 않아도 된다.
    그리고 내 풀이에서는 zip(*board)를 사용해서 행렬 전환을 하면서 열(col) 별로 리스트를 재구성했는데,
    이 풀이에서는 행렬전환 없이 board[][]를 사용해서 원소들을 사용했다.
    내 풀이보다 길이도 짧고 압축되어 있지만 이중 for을 사용했기 때문에 board나 moves의 길이가 길수록 효율이 낮아진다.
    len(board) = 5 & mobes = 1000인 경우,
    내 풀이는 1900번, 이 풀이는 30,000번을 수행한다.
    이 경우에는 간략함과 시간 중 어떤 것이 우선일까?

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -20,3 +20,23 @@ def solution(board, moves):
                 answer += 2 ## 사라진 인형의 수
     
     return answer
+
+# somone else's answer: 이중 for & pop()
+def solution(board, moves):
+    stacklist = [] ## basket과 같은 역할
+    answer = 0
+
+    for i in moves:
+        for j in range(len(board)): ## 이중 for loop을 이용해 전체를 순환
+            if board[j][i-1] != 0: ## 0인 경우는 스킵
+                stacklist.append(board[j][i-1])
+                board[j][i-1] = 0
+
+                if len(stacklist) > 1:
+                    if stacklist[-1] == stacklist[-2]:
+                        stacklist.pop(-1) ## basket = basket[:-2]과 같은 역할
+                        stacklist.pop(-1)
+                        answer += 2     
+                break
+
+    return answer
