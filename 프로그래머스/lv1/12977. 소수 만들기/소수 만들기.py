260044bff106828e1c4914d1a59acfd3b20d1ca7
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-24 18:24:04 +0900

    refactor: add remarks
    구상 순서
    1. 소수 판별 함수 생성 -> is_prime
    2. 세개의 수 더하기 -> 삼중 for 구문
    3. 소수의 개수 세기 -> 중복된 수는 한번에 판별해서 더하기
    
    나는 아직 일의 단계를 나누고 하나씩 처리하는 데에 더 익숙한 것 같다.
    전체를 보면서 한번에 처리할 수 있는 부분은 같이 처리하자
    3은 2에서 삼중 loop를 돌릴 때 하나씩 더하는 편이 더 간결했을 것이다.

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
@@ -1,4 +1,6 @@
-def is_prime(num, i = 2):
+# is_prime 함수 생성 & collections.Counter() & 삼중 for loop
+
+def is_prime(num, i = 2): ## 소수 여부 판별
     if num == 2:
         return True
     else:
@@ -15,10 +17,10 @@ def solution(nums):
     answer = 0
     sums = []
     
-    for i in range(len(nums)):
+    for i in range(len(nums)): ## 세개의 수 i, j, k를 더함
         for j in range(len(nums)):
             for k in range(len(nums)):
-                if i < j and j < k:
+                if i < j and j < k: ## 중복 제거
                     sums.append(nums[i] + nums[j] + nums[k])
     
     sum = collections.Counter(sums) ## 원소별 개수 출력
