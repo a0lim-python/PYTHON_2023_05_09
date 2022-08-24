5d2f4a26887c72a5ee6103e9efda8cb7cca474ec
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-24 15:36:56 +0900

    refactor: add remarks
    구상 순서
    1. 재귀함수를 이용해 n, m의 약수를 각각 구한 후 공약수를 추출 -> timeout
    2. n, m을 한번에 판별하여 공약수를 추출
    3. A * B = GCD * LCM
    4. answer[i]로 변수를 넣을 때는 그 자리에 기존 answer 값이 존재해야 함
    or answer = [], append.answer()을 사용 (헷갈리지 않기)

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,21 +1,23 @@
+# 재귀함수(recursive function) & math.prod & A * B = GCD * LCM
+
 def solution(n, m):
     import math
     
-    def measure(n, m, i = 2, result = [1]):
-        if n == 1 or m == 1:
+    def measure(n, m, i = 2, result = [1]): ## 공약수들을 추출
+        if n == 1 or m == 1: ## 1인 경우, 최대공약수 = 1
             return result
         else:
             while n >= i and m >= i:
-                if n % i == 0 and m % i == 0: # 공약수인 경우
+                if n % i == 0 and m % i == 0: ## 공약수인 경우
                     result.append(i)
-                    return measure(n // i, m // i, i = 2, result = result)
+                    return measure(n // i, m // i, i = 2, result = result) ## 재귀
                 else:
                     i += 1
             return result
                     
     answer = [0, 0]
     
-    answer[0] = math.prod(measure(n, m))
-    answer[1] = int(n * m / answer[0])
+    answer[0] = math.prod(measure(n, m)) 
+    answer[1] = int(n * m / answer[0]) ## LCM = n * m / GCD
 
     return answer
