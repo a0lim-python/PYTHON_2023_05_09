73d7e64f6624814b4869db48397824cb50de1372
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-24 12:43:40 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.22 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -0,0 +1,21 @@
+# numpy를 사용하지 않음
+def solution(board, moves):
+    answer = 0
+    basket = []
+    board = list(map(list,(zip(*board)))) ## 행렬 전환(형태: 리스트)
+    
+    for i in range(len(board)): ## 0 제거
+        while board[i][0] == 0:
+            board[i].remove(0)
+    
+    for i in moves:
+        if board[i - 1] != []:
+            basket.append(board[i - 1][0]) ## 위에 있는 인형을 basket으로 이동
+            del board[i - 1][0]
+        
+        if len(basket) > 1:
+            if basket[-1] == basket[-2]: ## 같은 인형이 쌓이면 바구니에서 삭제
+                basket = basket[:-2]
+                answer += 2 ## 사라진 인형의 수
+    
+    return answer
