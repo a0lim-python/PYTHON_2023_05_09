5a89b9b04b69efee119a801c28b20e27f38e950a
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-24 16:23:06 +0900

    refactor: add someone else's answer
    유클리드 호제법을 처음 알게 되었다.
    저 방법 하나만으로도 코드가 훨씬 간결해진다.
    많이 배워야겠다.

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -21,3 +21,23 @@ def solution(n, m):
     answer[1] = int(n * m / answer[0]) ## LCM = n * m / GCD
 
     return answer
+
+# someone else's answer: 유클리드 호제법
+'''
+def gcd(a, b):
+    while b > 0:
+        a, b = b, a % b
+    return a
+
+참고: https://velog.io/@jwisgenius/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%ED%8C%8C%EC%9D%B4%EC%8D%AC-%EC%B5%9C%EB%8C%80%EA%B3%B5%EC%95%BD%EC%88%98%EC%9C%A0%ED%81%B4%EB%A6%AC%EB%93%9C-%ED%98%B8%EC%A0%9C%EB%B2%95-%EC%B5%9C%EC%86%8C%EA%B3%B5%EB%B0%B0%EC%88%98
+'''
+
+def gcdlcm(a, b):
+    c, d = max(a, b), min(a, b)
+    t = 1
+    while t > 0:
+        t = c % d
+        c, d = d, t
+    answer = [c, int(a*b/c)]
+
+    return answer
