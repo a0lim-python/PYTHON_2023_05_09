ee1e73d57c23384c5be0a4a006512c7ee825a591
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-25 13:47:40 +0900

    refactor: add remarks
    구상 순서:
    1. 실패율 구하기 -> 모수 조정 필요 -> 이전 단계 사람들 삭제 -> for 문(비효율적..)
    2. 실패율 비교 -> enumerate로 실패율의 index 설정
    3. 실패율 낮은 순, 스테이지 단계가 낮은 순으로 정렬 -> sorted & lambda
    4. index로 설정되어있으므로 +1 필요

--- 프로그래머스/lv1/42889. 실패율/실패율.py
+++ 프로그래머스/lv1/42889. 실패율/실패율.py
@@ -1,12 +1,14 @@
+# set & count & sorted lambda
+
 def solution(N, stages):
     
     fail = [] ## 실패율
     for i in range(1, N + 1):
-        if i not in list(set(stages)):
+        if i not in list(set(stages)): # stages에 없다면 실패율 = 0(모두 성공)
             fail.append(0)
         else:
             fail.append(stages.count(i)/len(stages))
-            stages = [j for j in stages if j > i]
+            stages = [j for j in stages if j > i] # 해당 스테이지 사람들 삭제
         
     answer = sorted(list(enumerate(fail)), key = lambda x: (-x[1], x[0])) ## 실패율 낮은 순, 스테이지 단계가 낮은 순으로 정렬
     answer = [answer[i][0] + 1 for i in range(N)]
