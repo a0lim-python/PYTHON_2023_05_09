55a290d444d8a00061653cf96942a1512366894c
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-25 10:19:32 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.67 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.py
@@ -0,0 +1,13 @@
+def solution(s):
+    import string
+    lower = ''; upper = ''
+    
+    for i in s:
+        if s in string.ascii_lowercase:
+            lower += i
+        else:
+            upper += i
+            
+    answer = ''.join(sorted(lower, reverse = True) + sorted(upper, reverse = True))
+        
+    return answer
