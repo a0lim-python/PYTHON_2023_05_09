f308e29eda3c994b13eb7da1bd3ab310884f5926
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-25 10:28:42 +0900

    refactor: add someone else's answer
    기본적인 sorted의 순서가 대문자, 소문자 순이기 때문에 소문자/대문자를 나누어서 sorted한 후 합칠 필요가 없었다.

--- 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.py
+++ 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.py
@@ -13,3 +13,8 @@ def solution(s):
     answer = ''.join(sorted(lower, reverse = True) + sorted(upper, reverse = True))
         
     return answer
+
+# someone else's answer: ''.join(), sorted
+
+def solution(s):
+    return ''.join(sorted(s, reverse=True)) ## 기본 sorted 순서: 대문자 A-Z, 소문자 a-z
