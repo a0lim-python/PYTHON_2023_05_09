be3738e5a9778dc81a4a097e8117f53b715b7dbd
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-25 14:51:49 +0900

    [level 1] Title: 소수 찾기, Time: 228.66 ms, Memory: 17.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
@@ -0,0 +1,13 @@
+def solution(n):
+    ## 에라토스테네스의 체
+    answer = 0
+    nums = [True] * (n + 1)
+    nums[0] = False; nums[1] = False ## 0은 해당되지 않음 / 1은 소수가 아님
+    
+    for i in range(2, n + 1):
+        if nums[i] is True:
+            for j in range(2, (n // i) + 1): ## 소수 자신은 제외
+                nums[i * j] = False
+    
+    answer = nums.count(True)
+    return answer
