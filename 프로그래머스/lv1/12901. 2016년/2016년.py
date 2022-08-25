0a25f82b0daaa6f634558074594d7864f6ecbf8d
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-25 13:10:05 +0900

    refactor: add my other answer and remarks
    구상 순서
    1. 파이썬 내의 date, time 관련 함수 사용 -> datetime
    2. (출제 의도)일수 계산 및 나머지 활용 -> for & %

--- 프로그래머스/lv1/12901. 2016년/2016년.py
+++ 프로그래머스/lv1/12901. 2016년/2016년.py
@@ -1,12 +1,24 @@
+# datetime
+
+def solution(a, b):
+    import datetime
+    weekdays = {0:'MON', 1:'TUE', 2:'WED', 3:'THU', 4:'FRI', 5:'SAT', 6:'SUN'}
+    d = '2016-' + str(a) + '-' + str(b) ## '2016-a-b' 형식 구현
+    x = datetime.datetime.strptime(d, "%Y-%m-%d") ## 연, 월, 일에 맞는 datetime을 매칭
+    answer = weekdays[x.weekday()] ## weekday(): 요일 출력 (weekdays와 같게)
+    return answer
+
+# list & for & %
+
 def solution(a, b):
     days = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     weekday = ['THU', 'FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED'] ## 일자 % 7 이 작은 순서대로
     
     date = 0
-    for i in range(a - 1):
+    for i in range(a - 1): ## 직전 월까지
         date += days[i]
         
-    date += b
+    date += b ## 해당 월의 일 수
     answer = weekday[date % 7]
     
     return answer
