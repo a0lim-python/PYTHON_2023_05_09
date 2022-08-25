80d1ccff59d8f02de625870479c2c4917eb482b6
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-25 15:11:41 +0900

    refactor: add my other and someone else's answer
    # 에라토스테네스의 체 활용: while, for 문 & remove
    기본 에라토스테네스의 체는 n까지 존재하는 소수를 구하는 방법인데,
    이 문제에서 소수 판별까지는 불필요하므로 해당 단계를 뺐다.
    코드 목표는 완전 탐색 지양이었다.
    1 ~ n에서 소수를 포함한 소수의 배수들을 전부 삭제하고 answer += 1을 했다.
    그러면 nums[0]은 자동으로 그 다음 소수가 된다.
    문제는 그렇게 빼면, 삭제하고자 하는 배수의 개수를 모르기 때문에 while로 돌리기가 어렵다.
    결국 목표와 조금 다르게 while 안의 for문은 완전 탐색을 진행해서 아쉬웠다.
    그리고 전체 nums의 개수 자체를 줄여나가면서 while의 시간 복잡도를 낮추려고 했는데, 그래도 높아서 제출하지는 못했다.
    
    # someone else's answer
    이 풀이도 소수 판별 자체는 건너 뛰었다.
    그리고 내가 while 내의 for문에서 고민했던 배수의 개수를 range를 이용해서 찾아냈다.
    무엇보다 set으로 차집합을 사용해서 문제를 매우 간결하게 풀어냈다.
    좀 더 다각도로 생각해보자

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
@@ -1,6 +1,5 @@
 # 에라토스테네스의 체: 이중 for문 & True/False
 def solution(n):
-    ## 에라토스테네스의 체
     answer = 0
     nums = [True] * (n + 1)
     nums[0] = False; nums[1] = False ## 0은 해당되지 않음 / 1은 소수가 아님
@@ -12,3 +11,29 @@ def solution(n):
     
     answer = nums.count(True)
     return answer
+
+# 에라토스테네스의 체 활용: while, for 문 & remove -> timeerror
+
+def solution(n):
+    answer = 0
+    nums = list(range(2, n + 1))
+    
+    while nums != []:
+        answer += 1
+        prime = nums[0] # all[0]: 남아있는 소수의 최소값
+        
+        for i in range(1, max(nums) // prime + 1): ## 소수 및 소수의 배수 삭제 / 배수의 개수를 모르므로 for 문을 사용해서 완전 탐색 진행
+            if prime * i in nums:
+                nums.remove(prime * i)
+        
+    return answer
+
+# someone else's answer: 에라토스테네스의 체 & for문 & set(차집합)
+
+def solution(n):
+    num=set(range(2,n+1))
+
+    for i in range(2,n+1): ## 차집합으로 제거하므로 소수 여부 판별 불필요
+        if i in num:
+            num-=set(range(2*i,n+1,i)) ## i: 소수 / i의 배수를 삭제(n+1이 되기 전까지) / set: 차집합을 사용하여 해당 값을 리스트에서 제거
+    return len(num)
