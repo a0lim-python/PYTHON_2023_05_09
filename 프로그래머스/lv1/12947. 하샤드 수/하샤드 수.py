5cf2c1a3ef45829621facd0e94fe0cb9b6266e8f
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-25 10:10:49 +0900

    refactor: add someone else's answer
    return에서 비교연산자를 사용할 수 있다는 것을 배웠다.

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.py
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.py
@@ -6,3 +6,12 @@ def solution(x):
     else:
         answer = False
     return answer
+
+# someone else's answer 1: list comprehension & sum & str
+
+def Harshad(n):
+    return n % sum([int(c) for c in str(n)]) == 0
+
+## 1. list comprehension을 사용해서 바로 자릿수의 합을 구함
+## 2. int(c)의 합을 구하기 위해 sum을 사용
+## 3. return에서 비교연산자 사용 -> True / False 지정이 필요 없음
