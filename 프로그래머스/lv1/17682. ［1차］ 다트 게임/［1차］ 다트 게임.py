74aea5557570095585cfa034642ca35b73080b44
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-26 13:41:15 +0900

    refactor: add someone else's answers
    # someone else's answer 1
    숫자 처리에서 10이 까다로웠는데 replace로 간단하게 처리했다
    내 코드는 숫자 입력, bonus 적용, option 적용 순서였기 때문에 숫자인지 판별이 필요했다면
    이 코드는 bonus, option, else(숫자) 순서로 loop를 구성해서 bonus와 option을 제외하면 모두 숫자로 처리해서 판별이 필요 없다.
    
    # someone else's answer 2
    정규표현식으로 문자를 나누는 방법을 처음 알게 되었다.
    이것 하나로 그 많던 elif문들이 필요 없어지고, 마지막에 간단한 연산만 남았다.
    re 패키지가 좀 무거워서 보이는 것 보다는 속도가 느리겠지만 이렇게 간단하게 표현할 수 있다는 점이 참 대단한 것 같다.
    그리고 bonus와 option을 모두 dict로 만들어서 index 적용만 하는 점도 매력적으로 보인다.
    
    나는 리스트나 딕셔너리를 새로 작성하는 것을 좋아하지 않는 편인데(특히 수기로), 경우에 따라  더 효율적인 경우도 있으니 좀 더 사용해보자

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -23,3 +23,45 @@ def solution(dartResult):
                 dart[-1] *= -1
     answer = sum(dart)
     return answer
+
+# someone else's answer 1: replace, if else
+
+def solution(dartResult):
+    point = []
+    answer = []
+    dartResult = dartResult.replace('10','k')
+    point = ['10' if i == 'k' else i for i in dartResult] ## 10을 k로 변환 후 다시 10으로 저장 -> 1, 0으로 나뉘지 않게 방지
+    print(point)
+
+    i = -1
+    sdt = ['S', 'D', 'T']
+    for j in point:
+        if j in sdt :
+            answer[i] = answer[i] ** (sdt.index(j)+1) ## bonus를 index에 맞게 처리
+        elif j == '*':
+            answer[i] = answer[i] * 2
+            if i != 0 :
+                answer[i - 1] = answer[i - 1] * 2
+        elif j == '#':
+            answer[i] = answer[i] * (-1)
+        else: ## 숫자인 경우 append
+            answer.append(int(j))
+            i += 1 ## 숫자 입력 후 0으로 시작
+    return sum(answer)
+
+# soneone else's answer 2: 정규표현식(re.complie), findall
+
+import re
+
+def solution(dartResult):
+    bonus = {'S' : 1, 'D' : 2, 'T' : 3}
+    option = {'' : 1, '*' : 2, '#' : -1}
+    p = re.compile('(\d+)([SDT])([*#]?)') ## 점수, bonus, 옵션으로 나눔 ## \d+: 숫자(\d) 1번 이상 반복(+) ## [SDT]: S or D or T ## [*#]?: *이나 #중 하나(?: 아예 없거나 1개만 있을 때만 return)
+    dart = p.findall(dartResult) ## // [('1', 'D', ''), ('2', 'S', ''), ('0', 'T', '')]
+    for i in range(len(dart)):
+        if dart[i][2] == '*' and i > 0: ## 이전 점수에 적용
+            dart[i-1] *= 2
+        dart[i] = int(dart[i][0]) ** bonus[dart[i][1]] * option[dart[i][2]] ## 한번에 계산(option이 없는 경우도 포함)
+
+    answer = sum(dart)
+    return answer
