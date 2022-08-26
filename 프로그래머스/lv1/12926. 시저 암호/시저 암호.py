c3cb0749a1a1593005c5c105f72ea3870e0488d0
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-26 12:05:56 +0900

    [level 1] Title: 시저 암호, Time: 2.93 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.py
@@ -0,0 +1,14 @@
+def solution(s, n):
+    import string
+    
+    s = list(s)
+    up = string.ascii_uppercase; low = string.ascii_lowercase
+
+    for i in range(len(s)):
+        if s[i] in up:
+            s[i] = (up * 2)[up.index(s[i]) + n] ## z 이상으로 넘어가는 경우를 위해, up * 2로 리스트 붙임
+        elif s[i] in low * 2:
+            s[i] = (low * 2)[low.index(s[i]) + n]
+            
+    answer = ''.join(s)
+    return answer
