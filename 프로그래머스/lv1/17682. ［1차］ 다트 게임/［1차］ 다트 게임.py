059e7a6ece3b6f30dce2080ebcbcd6fcd7bb848a
a0lim <104348646+a0lim@users.noreply.github.com>
2022-08-26 13:14:41 +0900

    Update ［1차］ 다트 게임.py
    구상 순서
    1. dartResult를 탐색해서 숫자와 문자 구분 ->  try/except -> 이중리스트 형태로 저장
    2. 점수 3개에 대해 bonus: 'S', 'D', 'T' 적용
    3. 점수 3개에 대해 ## option: '*', '#'
    4. sum
    
    문제 1. 숫자 10
    이전 코드로는 10이 1과 0으로 나뉘어 저장됨
    -> if로 이전이 1이고 현재가 0인 경우를 예외처리
    
    문제 2. 시간초과
    이전 코드는 for문이 3개(dartResult로 구분, 점수 bonus, 점수 option) & 점수 입력 시, 그 인덱스를 idx 변수에 저장
    -> dartResult for 문 하나로 진행 -> try/except으로 점수(int)와 그 외를 구별 -> 이어서 해당 점수에 bonus와 option을 적용(점수, bonus, option이 순차적으로 진행되기 때문에 index를 지정하지 않아도 됨)
    -> 이 과정에서 불필요한 변수들을 삭제하여 최소화함 ex) dart[idx] -> dart[-1]

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -1,12 +1,14 @@
+#  try/except & for
+
 def solution(dartResult):
     dart = []
     for i in range(len(dartResult)):
-        try:
+        try: ## 점수(int)
             if dartResult[i] == '0' and dartResult[i - 1] == '1': ## 10인 경우
                 dart[-1] *= 10
             else:
                 dart.append(int(dartResult[i]))
-        except:
+        except: ## 그 외(str)
             ## bonus: 'S', 'D', 'T'
             if dartResult[i] == 'D':
                 dart[-1] **= 2
